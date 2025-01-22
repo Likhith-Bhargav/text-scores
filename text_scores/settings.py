b10fa6980ccf8e6e2fd05b3fbd7b57459f271143
likhith <sutapallilikhithbhargav@gmail.com>
2025-01-22 18:26:34 +0530

    with database integration

--- text_scores/settings.py
+++ text_scores/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'score', 
 ]
 
 MIDDLEWARE = [
@@ -74,11 +75,14 @@ WSGI_APPLICATION = 'text_scores.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'scores',
+        'USER': 'postgres',
+        'PASSWORD': 'Soch@2608',  # Replace with the actual password
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
