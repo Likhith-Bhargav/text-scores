9351d59e3525848673f5fe848bf5620e58ad93fc
likhith <sutapallilikhithbhargav@gmail.com>
2025-01-23 17:39:12 +0530

    correct deployment

--- text_scores/settings.py
+++ text_scores/settings.py
@@ -138,3 +138,22 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CSRF_COOKIE_SECURE = False
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/path/to/django/debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
