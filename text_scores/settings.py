7bdc9a4613373fcefe952890e5d1f17cf5c171af
likhith <sutapallilikhithbhargav@gmail.com>
2025-01-24 16:34:21 +0530

    correct deployment

--- text_scores/settings.py
+++ text_scores/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c4a@sh=*^!2&o7w7p16ar3*3gv_s&2=nsn)q71wxss!_m1526d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['likhith-bhargav.github.io', 'www.likhith-bhargav.github.io', '127.0.0.1']
 
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR / "static"]
 # Default primary key field type
